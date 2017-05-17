a9ae03ac2423868d57fa305a9d145964bb0276d1
Tom Sherman <tsherma4@jhu.edu>
2017-05-17 13:25:07 -0400

    added new data

--- R/RVsharing.R
+++ R/RVsharing.R
@@ -24,21 +24,32 @@ createNetwork <- function(ped, parents, founders, prior)
 
     # create bayesian network
     net <- grain(compileCPT(c(founderNodes, nonFounderNodes)))
-    net <- compile(net, root=as.character(which(ped$affected==1)))
+    net <- compile(net)#, root=as.character(which(ped$affected==1)))
     return(propagate(net))
 }
 
 # calculate the joint-marginal distribution of specified nodes
 marginalProb <- function(net, marginalNodes, nSimulations)
 {
-    # calculate exact distribution
-    if (missing(nSimulations))
+    marginalNodes <- as.character(marginalNodes)
+
+    if (missing(nSimulations)) # calculate exact distribution
     {
-        prob <- querygrain(net, as.character(marginalNodes), type='joint')
-        p0 <-  prob[matrix(rep(1,length(marginalNodes)),
-            ncol=length(marginalNodes))]
-        p1 <-  prob[matrix(rep(2,length(marginalNodes)),
-            ncol=length(marginalNodes))]
+        p0 <- p1 <- 1
+        net0 <- net1 <- net
+        for (n in marginalNodes)
+        {
+            if (p0 > 0) # prevents conditioning on zero prob events
+            {
+                p0 <- p0 * unname(querygrain(net0, n)[[1]][1])
+                net0 <- setEvidence(net0, n, '0')
+            }
+            if (p1 > 0)
+            {
+                p1 <- p1 * unname(querygrain(net1, n)[[1]][2])
+                net1 <- setEvidence(net1, n, '1')
+            }
+        }
     }
     else # calculate distribution from monte carlo simulations
     {
@@ -59,6 +70,7 @@ RVsharing <- function(ped, alleleFreq, nSimulations)
     parents <- sapply(ped$id, function(i) c(ped$findex[i], ped$mindex[i]))
     founders <- which(parents[1,] == 0)
     affected <- which(ped$affected == 1)
+    # TODO throw error if founders and affected overlap
     nAff <- length(affected)
     
     # calculate prior distribution based on allele frequency
@@ -67,13 +79,13 @@ RVsharing <- function(ped, alleleFreq, nSimulations)
 
     # create bayesian network from the pedigree
     net <- createNetwork(ped, parents, founders, prior)
+    print(max(sapply(net$rip$cliques, length)))
 
     # if allele frequency not given, assume 1 founder introduces variant
     if (missing(alleleFreq))
     {
         # each fraction component of probability
-        numer <- 0
-        denom <- 0
+        numer <- denom <- 0
 
         # condition on each founder introducing the variant
         net <- setEvidence(net, as.character(founders),
