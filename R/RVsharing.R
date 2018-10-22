ca7772af70378454b5b8a178810a0d7e453287a7
abureau <abureau6@gmail.com>
2018-10-22 11:31:31 -0400

    Adding splitPed argument to RVsharing

--- R/RVsharing.R
+++ R/RVsharing.R
@@ -33,13 +33,14 @@ NULL
 #' @param nSim number of simulations used in monte carlo calculation
 #' @param founderDist custom distribution among founders - only used
 #' when simulating probability with nSim
-#' @param useAffected allows the user to condition on seeing the variant
+#' @param useAffected a logical value indicating whether to condition on seeing the variant
 #' among the affected subjects instead of the final descendants
 #' @param kinshipOrder order of the polynomial approximation to the distribtion
 #' of the number of distinct alleles in the founders (d in Bureau et al.).
 #' Must be <= 5
+#' @param splitPed a logical value indicating whether to split the pedigree in subpedigrees below each founder to enable computations in pedigrees too large to be stored in a single Bayesian network
 #' @param ... allows for arguments in the style of v1.7
-#' @return sharing probability between all carriers in pedigree
+#' @return sharing probability between all carriers in pedigree, or if splitPed = TRUE, a vector of sharing probabilities for all subsets of the carriers
 #' @examples
 #' data("samplePedigrees")
 #' RVsharing(samplePedigrees$firstCousinPair)
@@ -57,7 +58,7 @@ kinshipOrder=5, ...)
 #' @aliases RVsharing
 setMethod('RVsharing', signature(ped='pedigree'),
 function(ped, carriers, alleleFreq, kinshipCoeff, nSim,
-founderDist, useAffected, kinshipOrder, ...)
+founderDist, useAffected, kinshipOrder, splitPed=FALSE, ...)
 {
     # needed for backwards compatibility with v1.7
     ped <- oldArgs(ped, list(...)$data, list(...)$dad.id, list(...)$mom.id)
@@ -83,6 +84,10 @@ founderDist, useAffected, kinshipOrder, ...)
     else if (!is.na(kinshipCoeff))
     {
         prob <- twoFounderSharingProb(procPed, kinshipCoeff, kinshipOrder)
+    }
+    else if (splitPed)
+    {
+        prob <- oneFounderSharingProbSplitting(procPed)
     }
     else
     {
