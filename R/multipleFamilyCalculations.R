6a0a68fff4c610ff08ad1292c46fa3cb74c6b4ea
Tom Sherman <tomsherman159@gmail.com>
2019-07-17 17:07:02 -0400

    Created a C++ backend for some calculations
    Some of the functions in multipleFamilyCalculations.R are far too
    inefficient when run on large data sets. Adding a C++ backend gives
    better run times in the short term and allows for more optimization
    work to be done in the long term. In order to prevent a barrier for
    future devlopment, the original R backend has been left in and now
    there is an additional parameter for choosing which backend to run
    the functions with (defaulting to C++). This will still allow easy
    prototyping of new methods in R.

--- R/multipleFamilyCalculations.R
+++ R/multipleFamilyCalculations.R
@@ -35,9 +35,11 @@
 #' and Ruczinski, I. (2014) Inferring rare disease risk variants based on
 #' exact probabilities of sharing by multiple affected relatives.
 #' Bioinformatics, 30(15): 2189-96, doi:10.1093/bioinformatics/btu198.
-multipleFamilyPValue <- function(sharingProbs, observedSharing, minPValue=0)
+multipleFamilyPValue <- function(sharingProbs, observedSharing, minPValue=0, backend='cpp')
 {
     # check all pedigrees are given
+    if (length(names(observedSharing)) == 0 | length(names(sharingProbs)) == 0)
+        stop('sharingProbs and observedSharing must be named vectors')
     if (!all(names(observedSharing) %in% names(sharingProbs)))
         stop('missing some pedigrees in sharingProbs argument')
 
@@ -46,6 +48,156 @@ multipleFamilyPValue <- function(sharingProbs, observedSharing, minPValue=0)
     sharingProbs <- unname(sharingProbs)
     observedSharing <- unname(observedSharing)
 
+    # calculate p-value with appropiate backend
+    if (backend == 'cpp')
+        multipleFamilyPValue_cpp(sharingProbs, observedSharing, minPValue)
+    else
+        multipleFamilyPValue_R(sharingProbs, observedSharing, minPValue)    
+}
+
+#' generalization of multipleFamilyPValue to multiple variants
+#' @export
+#'
+#' @description Computes a p-value for each variant sharing pattern
+#'  across families
+#' @details For each variant, the families which have all sequenced subjects
+#'  sharing the variant and the families which have some sequenced subjects
+#'  sharing the variant are recorded. These values are passed
+#'  to multipleFamilyPValue
+#' @param snpMat SnpMatrix
+#' @param famInfo data frame containing pedigree, member, father, mother,
+#' sex, affected fields for each sequenced subject
+#' @param sharingProbs vector of sharing probabilites, must be a named vector
+#' with famid's for each probability
+#' @param minorAllele vector specifying the minor allele of each variant
+#' @param filter criteria for filtering pvalues
+#' @param alpha parameter for filter
+#' @return list containing p-values and potential p-values for each variant
+multipleVariantPValue <- function(snpMat, famInfo, sharingProbs,
+minorAllele=NULL, filter=NULL, alpha=0, backend="cpp")
+{
+    if (is.null(minorAllele))
+    {
+        minorAllele <- sapply(colnames(snpMat), function(var)
+        {
+            ifelse(sum(snpMat[,var] == 1) < sum(snpMat[,var] == 3), 1, 3)
+        })
+    }
+    if (length(minorAllele) != ncol(snpMat))
+    {
+        stop("Mismatch between number of minor alleles and size of the SnpMatrix")
+    }
+    validVariants <- sapply(colnames(snpMat), function(var)
+    {
+        sum(snpMat[,var] == minorAllele[var] | snpMat[,var] == 2) > 0
+    })
+    message(paste0("Ignoring ", sum(!validVariants), " variants not present in any subject"))
+    snpMat <- snpMat[,validVariants]
+    minorAllele <- minorAllele[validVariants]
+
+    # subset snpMat to only affected subjects
+    if (nrow(snpMat) != length(famInfo$affected))
+        stop("dimension mismatch: snpMat and famInfo")
+    snpMat <- snpMat[famInfo$affected == 2,]
+
+    if (backend == "cpp")
+    {
+        multipleVariantPValue_cpp(snpMat@.Data, colnames(snpMat),
+            as.character(famInfo$pedigree), sharingProbs, filter, minorAllele, alpha)
+    }
+    else
+    {
+        multipleVariantPValue_R(snpMat, as.character(famInfo$pedigree), sharingProbs,
+            minorAllele, filter, alpha)
+    }
+}
+
+#' enrichment p-value across multiple families and variants
+#' @export
+#'
+#' @description Computes a p-value for all variants seen across all families
+#' @details For each variant, the families which have all sequenced subjects
+#' sharing the variant and the families which have some sequenced subjects
+#' sharing the variant are recorded. All unique (family, variant) pairs
+#' are accumulated into a single vector and passed to multipleFamilyPValue
+#' @param threshold minimum p-value threshold passed to multipleFamilyPValue
+#' @inheritParams multipleVariantPValue
+#' @return p-value
+#' @references Fu, J., Beaty, T.H., Scott, A.F., Hetmanski, J., Parker, M.M.,
+#' Bailey-Wilson, J.E., Marazita, M.L., et al. 2017. Whole Exome Association of
+#' Rare Deletions in Multiplex Oral Cleft Families. Genetic Epidemiology 41
+#' (1): 61–69. doi:10.1002/gepi.22010.
+enrichmentPValue <- function(snpMat, famInfo, sharingProbs, threshold=0, backend='cpp')
+{
+    # determine the minor allele for each variant
+    minorAllele <- sapply(colnames(snpMat), function(var)
+    {
+        ifelse(sum(snpMat[,var] == 1) < sum(snpMat[,var] == 3), 1, 3)
+    })
+    validVariants <- sapply(colnames(snpMat), function(var)
+    {
+        sum(snpMat[,var] == minorAllele[var] | snpMat[,var] == 2) > 0
+    })
+    message(paste0("Ignoring ", sum(!validVariants), " variants not present in any subject"))
+    snpMat <- snpMat[,validVariants]
+    minorAllele <- minorAllele[validVariants]
+
+    # subset snpMat to only affected subjects
+    if (nrow(snpMat) != length(famInfo$affected))
+        stop("dimension mismatch: snpMat and famInfo")
+    snpMat <- snpMat[famInfo$affected == 2,]
+
+    if (backend == 'cpp')
+    {
+        enrichmentPValue_cpp(snpMat@.Data, as.character(famInfo$pedigree), sharingProbs,
+            minorAllele, threshold)
+    }
+    else
+    {
+        enrichmentPValue_R(snpMat@.Data, as.character(famInfo$pedigree), sharingProbs,
+            minorAllele, threshold)
+    }
+}
+
+#' deprecated function
+#' @export
+#' @description This function is deprecated with version >= 2.0
+#' and should not be used, instead use multipleFamilyPValue
+#' @param vec a vector of names of all families where a variant is seen
+#' @param not a vector of names of families where not all affected subjects
+#' share the rare variant
+#' @param pshare.data a data frame with at least two of the following columns:
+#' pshare: vector of RV sharing probabilities
+#' ped.tocompute.vec: vector of names of the families whose sharing 
+#' probability is contained in pshare. The names in the arguments
+#' vec and not must be found in ped.tocompute.vec
+#' @return P-value of the exact rare variant sharing test requiring
+#' sharing by all affected subjects.
+#' @examples
+#' data(samplePedigrees)
+#' notSharedFams <- c(15159, 15053, 15157)
+#' famids <- sapply(samplePedigrees, function(p) p$famid[1])
+#' notShared <- famids %in% notSharedFams
+#' probs <- sapply(samplePedigrees, RVsharing)
+#' get.psubset(famids, notShared, data.frame(pshare=probs,
+#' ped.tocompute.vec=famids))
+get.psubset <- function(vec, not, pshare.data)
+{
+    warning(paste('this function is deprecated',
+        'and should not be used, instead use multipleFamilyPValue'))
+    names.vec <- pshare.data$ped.tocompute.vec
+    probs <- pshare.data$pshare[names.vec %in% vec]
+    probNames <- names.vec[names.vec %in% vec]
+    shared <- !(probNames %in% not)
+    names(probs) <- names(shared) <- probNames
+    return(multipleFamilyPValue(probs, shared))
+}
+
+#' R backend for multipleFamilyPValue calculation
+#' @inheritParams multipleFamilyPValue
+#' @return p-value
+multipleFamilyPValue_R <- function(sharingProbs, observedSharing, minPValue=0)
+{
     # probability of observed data
     true_pObserved <- prod(sharingProbs[observedSharing]) * 
         prod(1 - sharingProbs[!observedSharing])
@@ -88,66 +240,18 @@ multipleFamilyPValue <- function(sharingProbs, observedSharing, minPValue=0)
     return(pvalue)
 }
 
-#' convert snpMatrix to a list of vectors of sharing
+#' R backend for multipleVariantPValue calculation
 #' @inheritParams multipleVariantPValue
-#' @return list of boolean vectors indicating sharing pattern for each variant
-convertMatrix <- function(snpMat, famInfo, minorAllele=NA)
-{
-    mat <- snpMat@.Data
-    shareList <- sapply(colnames(mat), function(var)
-    {
-        ret <- c()
-        if (is.na(minorAllele))
-        {
-            minorCount <- ifelse(sum(mat[,var] == 1) < sum(mat[,var] == 3), 1, 3)
-        }
-        else 
-        {
-            minorCount <- ifelse(minorAllele[var] == 1, 1, 3)
-        }
-    
-        for (fid in unique(famInfo$pedigree))
-        {
-            aff_rows <- which(famInfo$pedigree == fid & famInfo$affected == 2)
-            aff_alleles <- mat[aff_rows, var]
-            if (!all(aff_alleles != minorCount & aff_alleles != 2))
-            {
-                ret <- c(ret, all(aff_alleles == minorCount | aff_alleles == 2))
-                names(ret)[length(ret)] <- fid
-            }
-        }
-        return(ret)
-    }, USE.NAMES=TRUE, simplify=FALSE)
-    return(shareList[!sapply(shareList, is.null)])
-}
-
-#' generalization of multipleFamilyPValue to multiple variants
-#' @export
-#'
-#' @description Computes a p-value for each variant sharing pattern
-#'  across families
-#' @details For each variant, the families which have all sequenced subjects
-#'  sharing the variant and the families which have some sequenced subjects
-#'  sharing the variant are recorded. These values are passed
-#'  to multipleFamilyPValue
-#' @param snpMat SnpMatrix
-#' @param famInfo data frame containing pedigree, member, father, mother,
-#' sex, affected fields for each sequenced subject
-#' @param sharingProbs vector of sharing probabilites, must be a named vector
-#' with famid's for each probability
-#' @param minorAllele vector specifying the minor allele of each variant
-#' @param filter criteria for filtering pvalues
-#' @param alpha parameter for filter
-#' @return list containing p-values and potential p-values for each variant
-multipleVariantPValue <- function(snpMat, famInfo, sharingProbs,
-minorAllele=NA, filter=NULL, alpha=0)
+#' @return list of p-values and potential p-values
+multipleVariantPValue_R <- function(snpMat, famIds, sharingProbs,
+minorAllele, filter=NULL, alpha=0)
 {
     # check inputs
     if (is.null(names(sharingProbs)))
         stop('sharingProbs must be a named vector')
 
     # convert matrix to list of families with each allele
-    shareList <- convertMatrix(snpMat@.Data, famInfo, minorAllele)
+    shareList <- convertMatrix(snpMat@.Data, famIds, minorAllele)
 
     # calculate potential p-values
     pot_pvals <- sapply(shareList, function(vec)
@@ -184,25 +288,13 @@ minorAllele=NA, filter=NULL, alpha=0)
     return(list(pvalues=pvals[!is.na(pvals)], potential_pvalues=pot_pvals))
 }
 
-#' enrichment p-value across multiple families and variants
-#' @export
-#'
-#' @description Computes a p-value for all variants seen across all families
-#' @details For each variant, the families which have all sequenced subjects
-#' sharing the variant and the families which have some sequenced subjects
-#' sharing the variant are recorded. All unique (family, variant) pairs
-#' are accumulated into a single vector and passed to multipleFamilyPValue
-#' @param threshold minimum p-value threshold passed to multipleFamilyPValue
-#' @inheritParams multipleVariantPValue
+#' R backend for enrichmentPValue calculation
+#' @inheritParams enrichmentPValue
 #' @return p-value
-#' @references Fu, J., Beaty, T.H., Scott, A.F., Hetmanski, J., Parker, M.M.,
-#' Bailey-Wilson, J.E., Marazita, M.L., et al. 2017. Whole Exome Association of
-#' Rare Deletions in Multiplex Oral Cleft Families. Genetic Epidemiology 41
-#' (1): 61–69. doi:10.1002/gepi.22010.
-enrichmentPValue <- function(snpMat, famInfo, sharingProbs, threshold=0)
+enrichmentPValue_R <- function(snpMat, famIds, sharingProbs, minorAllele, threshold=0)
 {
     # convert matrix to list of families with each allele
-    shareList <- convertMatrix(snpMat@.Data, famInfo)
+    shareList <- convertMatrix(snpMat@.Data, famIds, minorAllele)
 
     # make sharing events unique for each variant by renaming
     probs <- pattern <- c()
@@ -221,37 +313,27 @@ enrichmentPValue <- function(snpMat, famInfo, sharingProbs, threshold=0)
     return(ifelse(pval <= threshold, threshold, pval))
 }
 
-#' deprecated function
-#' @export
-#' @description This function is deprecated with version >= 2.0
-#' and should not be used, instead use multipleFamilyPValue
-#' @param vec a vector of names of all families where a variant is seen
-#' @param not a vector of names of families where not all affected subjects
-#' share the rare variant
-#' @param pshare.data a data frame with at least two of the following columns:
-#' pshare: vector of RV sharing probabilities
-#' ped.tocompute.vec: vector of names of the families whose sharing 
-#' probability is contained in pshare. The names in the arguments
-#' vec and not must be found in ped.tocompute.vec
-#' @return P-value of the exact rare variant sharing test requiring
-#' sharing by all affected subjects.
-#' @examples
-#' data(samplePedigrees)
-#' notSharedFams <- c(15159, 15053, 15157)
-#' famids <- sapply(samplePedigrees, function(p) p$famid[1])
-#' notShared <- famids %in% notSharedFams
-#' probs <- sapply(samplePedigrees, RVsharing)
-#' get.psubset(famids, notShared, data.frame(pshare=probs,
-#' ped.tocompute.vec=famids))
-get.psubset <- function(vec, not, pshare.data)
+#' convert snpMatrix to a list of vectors of sharing
+#' @inheritParams multipleVariantPValue
+#' @return list of boolean vectors indicating sharing pattern for each variant
+convertMatrix <- function(snpMat, famIds, minorAllele)
 {
-    warning(paste('this function is deprecated',
-        'and should not be used, instead use multipleFamilyPValue'))
-    names.vec <- pshare.data$ped.tocompute.vec
-    probs <- pshare.data$pshare[names.vec %in% vec]
-    probNames <- names.vec[names.vec %in% vec]
-    shared <- !(probNames %in% not)
-    names(probs) <- names(shared) <- probNames
-    return(multipleFamilyPValue(probs, shared))
+    shareList <- sapply(colnames(snpMat), function(var)
+    {
+        ret <- c()
+        for (fid in unique(famIds))
+        {
+            famRows <- which(famIds == fid)
+            famAlleles <- snpMat[famRows, var]
+            if (!all(famAlleles != minorAllele[var] & famAlleles != 2))
+            {
+                ret <- c(ret, all(famAlleles == minorAllele[var] | famAlleles == 2))
+                names(ret)[length(ret)] <- fid
+            }
+        }
+        if (length(ret) == 0)
+            stop("found a variant not seen in any subject")
+        return(ret)
+    }, USE.NAMES=TRUE, simplify=FALSE)
+    return(shareList[!sapply(shareList, is.null)])
 }
-
---
+++ src/multipleFamilyCalculations.cpp
@@ -0,0 +1,265 @@
+#include <Rcpp.h>
+
+#include <map>
+
+typedef std::vector< std::vector<unsigned> > FamilyRowMap;
+
+static double leafSum(unsigned ndx, double product, const Rcpp::NumericVector &sharingProbs,
+const double observedPValue);
+
+static double sumBranches(unsigned ndx, double product, const Rcpp::NumericVector &sharingProbs,
+const double observedPValue);
+
+bool contains(const Rcpp::CharacterVector &vec, const std::string &str)
+{
+    for (unsigned i = 0; i < vec.size(); ++i)
+    {
+        if (Rcpp::as<std::string>(vec[i]) == str)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void checkInputs(const Rcpp::IntegerMatrix &alleles,
+const Rcpp::CharacterVector &variants, const Rcpp::CharacterVector &famIds,
+const Rcpp::NumericVector &sharingProbs, const std::string &filter,
+const Rcpp::NumericVector &minorAllele, double alpha)
+{
+    if (variants.size() != alleles.ncol())
+    {
+        Rcpp::stop("Mismatch between variant names and the SnpMatrix");
+    }
+    if (famIds.size() != alleles.nrow())
+    {
+        Rcpp::stop("Mistmatch between family ids and the SnpMatrix");
+    }
+    if (!filter.empty() && (alpha <= 0.0 || alpha > 1.0))
+    {
+        Rcpp::stop("Invalid alpha value for chosen filter");
+    }
+    for (unsigned i = 0; i < famIds.size(); ++i)
+    {
+        if (!contains(sharingProbs.attr("names"), Rcpp::as<std::string>(famIds[i])))
+        {
+            Rcpp::stop("Can't find family ID in the sharing probabilities");
+        }
+    }
+}
+
+// This maps each family id to all of the rows in the SnpMatrix that contain an affected
+// subject of that family id. The map is indexed based on the sharingProbs vector,
+// i.e. if you want to find the rows corresponding to the subjects in sharingProbs[i] you
+// can use familyRowMap[i]
+FamilyRowMap getFamilyRowMap(const Rcpp::NumericVector &sharingProbs,
+const Rcpp::CharacterVector &famIds)
+{
+    FamilyRowMap familyRowMap;
+    Rcpp::CharacterVector uniqueFamIds = sharingProbs.attr("names");
+    for (unsigned i = 0; i < uniqueFamIds.size(); ++i)
+    {
+        familyRowMap.push_back(std::vector<unsigned>());
+        std::string thisFamId = Rcpp::as<std::string>(uniqueFamIds[i]);
+        for (unsigned j = 0; j < famIds.size(); ++j) // loop over rows of SnpMatrix
+        {
+            if (famIds[j] == thisFamId)
+            {
+                familyRowMap[i].push_back(j);
+            }
+        }
+    }
+    return familyRowMap;
+}
+
+static bool minorAllelePresent(const Rcpp::IntegerMatrix &snpMat, int minorAllele,
+const std::vector<unsigned> &rowNdx, unsigned colNdx)
+{
+    for (unsigned i = 0; i < rowNdx.size(); ++i)
+    {
+        if (snpMat(rowNdx[i], colNdx) == 2 || snpMat(rowNdx[i], colNdx) == minorAllele)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+static bool minorAlleleShared(const Rcpp::IntegerMatrix &snpMat, int minorAllele,
+const std::vector<unsigned> &rowNdx, unsigned colNdx)
+{
+    for (unsigned i = 0; i < rowNdx.size(); ++i)
+    {
+        if (snpMat(rowNdx[i], colNdx) != minorAllele && snpMat(rowNdx[i], colNdx) != 2)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+Rcpp::NumericVector calculatePotentialPValues(const Rcpp::IntegerMatrix &snpMat,
+const Rcpp::CharacterVector &famIds, const Rcpp::NumericVector &sharingProbs,
+const Rcpp::NumericVector &minorAllele, const FamilyRowMap &familyRowMap)
+{
+    // calculate potential p-values
+    unsigned nVariants = static_cast<unsigned>(snpMat.ncol());
+    Rcpp::NumericVector ppvals(nVariants, 1.0);
+    for (unsigned i = 0; i < nVariants; ++i)
+    {
+        for (unsigned j = 0; j < sharingProbs.size(); ++j)
+        {
+            if (minorAllelePresent(snpMat, minorAllele[i], familyRowMap[j], i))
+            {
+                ppvals[i] *= sharingProbs[j];
+            }
+        }
+    }
+    return ppvals;
+}
+
+double findPValueCutoff(const Rcpp::NumericVector &ppvals, const std::string &filter, double alpha)
+{
+    if (filter.empty())
+    {
+        return 1.0;
+    }
+    Rcpp::NumericVector sorted = Rcpp::clone(ppvals);
+    sorted.sort();
+    unsigned ndx = 0;
+    while (sorted[ndx] < alpha / static_cast<double>(ndx + 1))
+    {
+        ++ndx;
+    }
+    return sorted[ndx - 1];
+}
+
+static Rcpp::NumericVector filterVector(const Rcpp::NumericVector &vecIn, double minVal)
+{
+    Rcpp::NumericVector vecOut;
+    for (unsigned i = 0; i < vecIn.size(); ++i)
+    {
+        if (vecIn[i] >= minVal)
+        {
+            vecOut.push_back(vecIn[i]);
+        }
+    }
+    return vecOut;
+}
+
+static double leafSum(unsigned ndx, double product, const Rcpp::NumericVector &sharingProbs,
+const double observedPValue)
+{
+    return (product < observedPValue + 0.001)
+        ? product
+        : sumBranches(ndx + 1, product, sharingProbs, observedPValue);
+}
+
+static double sumBranches(unsigned ndx, double product, const Rcpp::NumericVector &sharingProbs,
+const double observedPValue)
+{
+    if (ndx >= sharingProbs.size())
+    {
+        return 0;
+    }
+    double leftProduct = sharingProbs[ndx] * product;
+    double rightProduct = (1 - sharingProbs[ndx]) * product;
+    return leafSum(ndx, leftProduct, sharingProbs, observedPValue)
+        + leafSum(ndx, rightProduct, sharingProbs, observedPValue);
+}
+
+// [[Rcpp::export]]
+double multipleFamilyPValue_cpp(const Rcpp::NumericVector &sharingProbs,
+const Rcpp::LogicalVector &observedSharing, double minPValue=0.0)
+{
+    double observedPValue = 1.0;
+    for (unsigned i = 0; i < observedSharing.size(); ++i)
+    {
+        observedPValue *= observedSharing[i] ? sharingProbs[i] : (1.0 - sharingProbs[i]);
+    }
+    if (observedPValue == 0.0)
+    {
+        return 0.0;
+    }
+    
+    double pvalue = 1.0;
+    double burnInObservedPValue = 1.0;
+    while (pvalue > minPValue && burnInObservedPValue > observedPValue)
+    {
+        burnInObservedPValue = std::max(burnInObservedPValue / 10.0, observedPValue);
+        pvalue = sumBranches(0, 1, sharingProbs, burnInObservedPValue);
+    }
+    return pvalue;
+}
+
+Rcpp::NumericVector calculatePValues(const Rcpp::IntegerMatrix &snpMat,
+const Rcpp::CharacterVector &famIds, 
+const Rcpp::NumericVector &sharingProbs, const Rcpp::NumericVector &minorAllele,
+double cutoff, const FamilyRowMap &familyRowMap, const Rcpp::NumericVector &ppvals)
+{
+    unsigned nVariants = static_cast<unsigned>(snpMat.ncol());
+    Rcpp::NumericVector pvals(nVariants, -1.0);
+    for (unsigned i = 0; i < nVariants; ++i)
+    {
+        if (ppvals[i] <= cutoff)
+        {
+            Rcpp::NumericVector probs;
+            Rcpp::LogicalVector pattern;
+            for (unsigned j = 0; j < familyRowMap.size(); ++j)
+            {
+                if (minorAllelePresent(snpMat, minorAllele[i], familyRowMap[j], i))
+                {
+                    probs.push_back(sharingProbs[j]);
+                    pattern.push_back(minorAlleleShared(snpMat, minorAllele[i], familyRowMap[j], i));
+                }
+            }
+            pvals[i] = multipleFamilyPValue_cpp(probs, pattern);
+        }
+    }
+    return pvals;
+}
+
+// [[Rcpp::export]]
+Rcpp::List multipleVariantPValue_cpp(const Rcpp::IntegerMatrix &alleles,
+const Rcpp::CharacterVector &variants, const Rcpp::CharacterVector &famIds,
+const Rcpp::NumericVector &sharingProbs,
+const Rcpp::Nullable<Rcpp::CharacterVector> &rfilter,
+const Rcpp::NumericVector &minorAllele,
+double alpha)
+{
+    std::string filter = (rfilter.isNull()) ? "" : Rcpp::as<std::string>(rfilter);
+    checkInputs(alleles, variants, famIds, sharingProbs, filter, minorAllele, alpha);
+    FamilyRowMap familyRowMap(getFamilyRowMap(sharingProbs, famIds));
+    Rcpp::NumericVector potPValues = calculatePotentialPValues(alleles, famIds,
+        sharingProbs, minorAllele, familyRowMap);
+    double cutoff = findPValueCutoff(potPValues, filter, alpha);
+    Rcpp::NumericVector pvalues = calculatePValues(alleles, famIds, sharingProbs,
+        minorAllele, cutoff, familyRowMap, potPValues);
+    pvalues = filterVector(pvalues, 0.0);
+    return Rcpp::List::create(
+        Rcpp::Named("pvalues") = pvalues,
+        Rcpp::Named("potential_pvalues") = potPValues
+    );
+}
+
+// [[Rcpp::export]]
+double enrichmentPValue_cpp(const Rcpp::IntegerMatrix &snpMat,
+const Rcpp::CharacterVector &famIds, const Rcpp::NumericVector &sharingProbs,
+const Rcpp::NumericVector &minorAllele, double threshold)
+{
+    Rcpp::NumericVector probs;
+    Rcpp::LogicalVector pattern;
+    FamilyRowMap familyRowMap(getFamilyRowMap(sharingProbs, famIds));
+    for (unsigned i = 0; i < snpMat.ncol(); ++i)
+    {
+        for (unsigned j = 0; j < sharingProbs.size(); ++j)
+        {
+            if (minorAllelePresent(snpMat, minorAllele[i], familyRowMap[j], i))
+            {
+                probs.push_back(sharingProbs[j]);
+                pattern.push_back(minorAlleleShared(snpMat, minorAllele[i], familyRowMap[j], i));
+            }
+        }
+    }
+    return multipleFamilyPValue_cpp(probs, pattern, threshold);
+}
