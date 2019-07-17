ee53496a96a0ee158b2cb657c772e56d59355d5b
Tom Sherman <tomsherman159@gmail.com>
2019-07-17 17:13:26 -0400

    Added tests for new C++ backend
    It is critical that the C++ and R backends agree on the results.
    These new tests ensure that both backends will give the exact same
    result and are critical tests to run when optimizing in order to
    prevent bugs from being introduced to the C++ backend.

--- tests/testthat/test_multipleFamilyCalculations.R
+++ tests/testthat/test_multipleFamilyCalculations.R
@@ -1,31 +1,77 @@
 context('multiple family calculations')
 
-test_that('multiple family p-value',
+test_that('multipleFamilyPValue',
 {
+    # test specific result
     data(samplePedigrees)
     probs <- sapply(samplePedigrees, RVsharing)
     obs <- c(rep(TRUE, 2), rep(FALSE, 7))
     names(obs) <- names(probs)
     expect_equal(multipleFamilyPValue(probs, obs), 0.0135, tol=0.001)
+
+    # test error handling
+    expect_error(multipleFamilyPValue(unname(probs), obs))
+    expect_error(multipleFamilyPValue(probs, unname(obs)))
+    names(obs) <- LETTERS[1:length(probs)]
+    expect_error(multipleFamilyPValue(probs, obs))
+        
+    # test agreement between each backend
+    for (n in 1:100)
+    {
+        obs <- sample(c(TRUE, FALSE), length(probs), replace=TRUE)
+        names(obs) <- names(probs)
+        prob_R <- multipleFamilyPValue(probs, obs, backend='r')
+        prob_CPP <- multipleFamilyPValue(probs, obs, backend='cpp')
+        expect_equal(prob_R, prob_CPP)
+    }
 })
 
-
-test_that('multiple variant p-value',
+test_that('multipleVariantPValue',
 {
+    # create test data
     pedfile <- system.file("extdata/sample.ped.gz", package="RVS")
-    sample <- snpStats::read.pedfile(pedfile, snps=paste('variant', LETTERS[1:20], sep='_'))
+    snpMat <- snpStats::read.pedfile(pedfile, snps=paste('variant', LETTERS[1:20], sep='_'))
     A_fams <- lapply(1:3, function(i) samplePedigrees$firstCousinPair)
     B_fams <- lapply(1:3, function(i) samplePedigrees$secondCousinPair)
     C_fams <- lapply(1:3, function(i) samplePedigrees$firstCousinTriple)
     D_fams <- lapply(1:3, function(i) samplePedigrees$secondCousinTriple)
     fams <- c(A_fams, B_fams, C_fams, D_fams)
-    famids <- unique(sample$fam$pedigree)
+    famids <- unique(snpMat$fam$pedigree)
     for (i in 1:12)
     {
         fams[[i]]$famid <- rep(famids[i], length(fams[[i]]$id))
     }
     sharingProbs <- suppressMessages(RVsharing(fams))
-    result <- multipleVariantPValue(sample$genotypes, sample$fam, sharingProbs)
-    expect_equal(length(result$pvalues), 20)
+
+    # test consistency of backends across all variants
+    result_R <- multipleVariantPValue(snpMat$genotypes, snpMat$fam, sharingProbs, backend='r')
+    result_CPP <- multipleVariantPValue(snpMat$genotypes, snpMat$fam, sharingProbs, backend='cpp')
+    expect_true(all(result_R$pvalues == result_CPP$pvalues))
+    expect_true(all(result_R$potential_pvalues == result_CPP$potential_pvalues))
 })
+
+test_that('enrichmentPValue',
+{
+    # create test data
+    data(samplePedigrees)
+    pedfile <- system.file("extdata/sample.ped.gz", package="RVS")
+    snpMat <- snpStats::read.pedfile(pedfile, snps=paste('variant', LETTERS[1:20], sep='_'))
+    A_fams <- lapply(1:3, function(i) samplePedigrees$firstCousinPair)
+    B_fams <- lapply(1:3, function(i) samplePedigrees$secondCousinPair)
+    C_fams <- lapply(1:3, function(i) samplePedigrees$firstCousinTriple)
+    D_fams <- lapply(1:3, function(i) samplePedigrees$secondCousinTriple)
+    fams <- c(A_fams, B_fams, C_fams, D_fams)
+    famids <- unique(snpMat$fam$pedigree)
+    for (i in 1:12)
+    {
+        fams[[i]]$famid <- rep(famids[i], length(fams[[i]]$id))
+    }
+    sharingProbs <- suppressMessages(RVsharing(fams))
+
+    # test specific result for both backends
+    pval <- enrichmentPValue(snpMat$genotypes, snpMat$fam, sharingProbs, backend='r')
+    expect_equal(pval, 0.124, tolerance=1e-3)
+    pval <- enrichmentPValue(snpMat$genotypes, snpMat$fam, sharingProbs, backend='cpp')
+    expect_equal(pval, 0.124, tolerance=1e-3)
+})  
 
